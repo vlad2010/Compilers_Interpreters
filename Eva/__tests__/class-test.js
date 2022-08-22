4859068b8e6e3cfd11f2eb395331a654036e286f
Vlad2010 <vlad2010@outlook.com>
2022-08-22 20:21:51 +0100

    Eva. Fixed test for Lection 15

--- Eva/__tests__/class-test.js
+++ Eva/__tests__/class-test.js
@@ -28,48 +28,34 @@ module.exports = (eva) => {
    	`
     , 30);
 
-    test(eva, 
-   	`
-   		(class Point null
-   		    (begin
 
-   		      (def constructor (this x y)
-   		      	 (begin
-                    (set (prop this x ) x)
-                    (set (prop this y ) y)
-   		      	 )
-   		      )
 
-   		      (def calc (this)
-   		      	(+ (prop this x) (prop this y))
-   		      )
-   		   )
-   		)
+   test(eva, 
+   `
+      (class Point3D Point
+          (begin
 
-   		(class Point3D Point
-   		    (begin
+            (def constructor (this x y z)
+                (begin
+                   ((prop (super Point3D) constructor) this x y)
+                 (set (prop this z) z)
+                )
+            )
 
-   		      (def constructor (this x y z)
-   		      	 (begin
-   		      	    ((prop (super Point3D) constructor) this x y)
-                    (set (prop this z) z)
-   		      	 )
-   		      )
+            (def calc (this)
+               (+ ((prop (super Point3D) calc) this)
+               
+                  (prop this z)
+               )
+            )
+         )
+      )
 
-   		      (def calc (this)
-   		      	(+ ((prop (super Point3D) calc) this)
-   		      	
-   		      	   (prop this z)
-   		      	)
-   		      )
-   		   )
-   		)
+      (var p (new Point3D 10 20 30))
 
-   		(var p (new Point3D 10 20 30))
-
-   		((prop p calc ) p)
-   	`
-    , 60);
+      ((prop p calc ) p)
+   `
+  , 60);
 
 
 };
